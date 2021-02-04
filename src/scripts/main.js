cb1efb0abe78987f2a5e6d8b90d71943555c7fc7
Vad1kRed <totalgol2015@gmail.com>
2021-02-04 13:52:26 +0200

    Last commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,5 @@ const basketMenu = document.querySelector('.basket__menu');
 
 basketBox.addEventListener('click', () => {
     basketMenu.classList.toggle('basket__menu--active');
+    basketBox.style.minWidth = basketMenu.offsetWidth + 'px';
 });
--- src/scripts/role.js
+++ src/scripts/role.js
@@ -1,4 +1,15 @@
 const infoContainer = document.querySelector('.info-container');
+const ingredientContainer = document.querySelector('.ingredient-container');
+
+new Swiper(ingredientContainer, {
+    loop: true,
+    spaceBetween: 25,
+    slidesPerView: 5,
+    navigation: {
+        nextEl: '.ingredient-button-next',
+        prevEl: '.ingredient-button-prev'
+    }
+})
 
 new Swiper(infoContainer, {
     pagination: {
