21da2dd11be57ca0a7873e3cbe379aff56cd7c6e
Vad1kRed <totalgol2015@gmail.com>
2021-02-09 17:28:34 +0200

    Last commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,6 +15,7 @@ const html = document.querySelector('html');
 const body = document.querySelector('body');
 
 const burger = document.querySelector('.burger');
+const blur = document.querySelector('.blur');
 const mobile = document.querySelector('.mobile');
 const mobileBox = document.querySelector('.mobile__box');
 const mobileBody = document.querySelector('.mobile__body');
@@ -23,6 +24,7 @@ burger.addEventListener('click', () => {
     html.classList.contains('hidden') ? html.removeAttribute('class') : html.classList.add('hidden');
 
     burger.classList.toggle('burger--active');
+    blur.classList.toggle('blur--active');
     mobile.classList.toggle('mobile--active');
     mobileBox.classList.toggle('mobile__box--active');
     mobileBody.classList.toggle('mobile__body--active');
